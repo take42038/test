5485e4c8ad58e34e3f2bb9b135fa822ea919218f
take42038 <take42038@gmail.com>
2017-10-14 18:00:30 +0900

    Help World追加

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 二宮武 on 2017/10/14.
+//  Copyright © 2017年 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
