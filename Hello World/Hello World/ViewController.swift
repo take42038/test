72925981a99c62eb587bc0fa5f24a3a3c52b33d3
take42038 <take42038@gmail.com>
2017-10-15 23:11:28 +0900

    ボタン＆ラベル　テスト

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //@IBOutlet var labeltest :UILabel!
+    //@IBOutlet var buttonTest :UIButton!
+    
+    @IBOutlet weak var labeltest: UILabel!
+    @IBOutlet weak var buttontest: UIButton!
+    
+    var count = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello World!")
+
+        labeltest.text = "Swift Test"
+        
+        buttontest.setTitle("Button", for:UIControlState.normal)
+        
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        count += 1
+        if(count%2 == 0){
+            labeltest.text = ("Swift Test")
+        }
+        else{
+            labeltest.text = ("tapped !")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
